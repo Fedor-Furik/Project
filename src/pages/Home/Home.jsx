f65f5f7052916015e9b6e8e5feb9d062aa049d43
Furik-Fedor <fedja2108100@gmail.com>
2023-12-09 15:56:34 +0300

    hdf

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,16 @@
 import "./Home.css"
+import Yammi from "./assets/HomeBlud.png"
 export default function Home() {
-    return <div><p className="pH">Home</p></div>
+    return(
+        <div className="ContainH">
+            <div className="BlockH1">
+                <div className="PodBlH11"><p>Доставка готовой еды из фермерских продуктов!</p></div>
+                <img className="hYamI" src={Yammi} alt="" />
+            </div>
+            <div className="BlockH2">
+            </div>
+            <div className="BlockH3">
+            </div>
+        </div>
+    )
 }
