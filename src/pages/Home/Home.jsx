117e54f20a5b6794a8a87d07e3f2bf37a9376c3e
Furik-Fedor <fedja2108100@gmail.com>
2023-12-09 16:14:40 +0300

    main

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import "./Home.css"
 import Yammi from "./assets/HomeBlud.png"
+import Toch from "./assets/Union.png"
 export default function Home() {
     return(
         <div className="ContainH">
             <div className="BlockH1">
-                <div className="PodBlH11"><p>Доставка готовой еды из фермерских продуктов!</p></div>
+                <div className="PodBlH11">
+                    <p>Доставка готовой еды из фермерских продуктов!</p>
+                    <img className="Backrooms" src={Toch} alt="" />
+                    <p className="numberPB1">+7 (499) 841-67-29</p>
+                    <h6>delivery@midas.rest</h6>
+                </div>
                 <img className="hYamI" src={Yammi} alt="" />
             </div>
             <div className="BlockH2">
